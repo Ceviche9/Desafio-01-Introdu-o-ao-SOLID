c6d65cd534c25d4dc2db62137657cf039e69f7d3
Tundê <ayotunde_sales@hotmail.com>
2022-02-11 17:37:22 -0300

    Chapter II: Desafio 01

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from "express";
+import { User } from "modules/users/model/User";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
@@ -6,7 +7,15 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+
+      const user: User = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
